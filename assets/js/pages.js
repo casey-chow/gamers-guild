a87fedb05789e4e65d58b664936dc26edde31ec9
Casey Chow <casey.chow@gmail.com>
2012-05-25 12:10:02 -0400

    Add keybindings, bugfixes, smarter hash handling to page javascript

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,7 +1,12 @@
 $(function() {
 
+  var last_location;
+
+  /* Function Definitions
+   * ----------------------------------------------- */
+
   function page_name ($page) {
-    return $page.attr('id').split('-')[1];//Expected: #page-home
+    return $page.attr('id').split('-')[1]; //Expected: #page-home
   }
 
   function move_navigation($page) {
@@ -11,14 +16,19 @@ $(function() {
     console.log('Moving nav to', page_name($page));
 
     $nav
-      .removeClass() //remove all classes
+      .removeClass() // remove all classes
       .addClass('is-' + page_name($page))
-      .animate({ top: yoffset }, 'fast'); //pull the nav down
+      .animate({ top: yoffset }, 200); // pull the nav down
+
+    last_location = page_name($page);
   }
 
   function scroll_to(position, duration, easing, callback) {
-    position = (position === 'top') ? 0 : position;
+    if ( position.jquery ) { position = position.offset().top + 1; }
+    if ( position === 'top' ) { position = 0; }
+
     $('html,body').animate({ scrollTop: position }, 'slow', easing, callback);
+    // TODO: Change hash on scroll
   }
 
   $.waypoints.settings.scrollThrottle = 15;
@@ -34,7 +44,6 @@ $(function() {
     move_navigation($page);
   }, { 
     offset: function() {
-      //var winheight = $.waypoints('viewportHeight');
       //FIXME doesn't work right in the up direction
       if ( $(this).is('#page-home') ) { return -25; }
       else if ( location.hash !== old_hash ) { //is a hashchange
@@ -44,6 +53,67 @@ $(function() {
     }
   });
 
+  // Catch it if a hash change doesn't move the navigation
+  $(window).hashChange(function() {
+    var page_name = location.hash.slice(1);
+    setTimeout(function() {
+      if (last_location !== page_name) {
+        move_navigation($('#' + page_name));
+      }
+    }, 201);
+  });
+
+  /* Moving between Pages
+   * ----------------------------------------- */
+
+  // by keys
+  $(document).keydown(function(evt) {
+    if (evt.keyCode < 37 && evt.keyCode > 40) { return; }
+
+    var $win = $(window);
+    var viewport_top = $win.scrollTop();
+    var viewport_bot = viewport_top + $win.height();
+    var $current, $target;
+
+    // find first page in viewport
+    // theoretically, only one top should be in the viewport at a time
+    $('.page-section').each(function() {
+      var $this = $(this);
+      var offset = $this.offset().top;
+      if ( offset >= viewport_top && offset < viewport_bot ) {
+        $current = $this;
+      }
+    });
+
+    switch(evt.keyCode) {
+      case 37: // left
+      case 38: // up
+        $target = $current.prev();
+        break;
+      case 39: // right
+      case 40: // down
+        $target = $current.next();
+        break;
+    }
+
+    if (!$target) { return; }
+
+    // scroll to the page identified
+    scroll_to($target);
+    setTimeout(function() {
+      if (last_location !== page_name($target)) {
+        move_navigation($('#page-' + page_name($target)));
+      }
+    }, 401);
+  });
+
+  /* Changing Hash on Scroll
+   * ------------------------------------------- */
+
+  // TODO: function that changes the hash on scroll
+
+  // If there's no hash, assume it should be at home
   if (!location.hash) { $('#navigation').addClass('is-home'); }
   else { move_navigation($(location.hash)); }
+
 });
