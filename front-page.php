5bfbc2bd85576bf01b29fa8c5490e3685932bb06
Casey Chow <casey.chow@gmail.com>
2012-06-25 14:26:25 -0400

    Add front page multipage functionality

---
+++ front-page.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+  <?php home_pages(); ?>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -18,30 +18,21 @@
     }
     add_action('init', 'removeHeadLinks');
     remove_action('wp_head', 'wp_generator');
-    
-  if (function_exists('register_sidebar')) {
-    register_sidebar(array(
-      'name' => __('Sidebar Widgets','html5reset' ),
-      'id'   => 'sidebar-widgets',
-      'description'   => __( 'These are widgets for the sidebar.','html5reset' ),
-      'before_widget' => '<div id="%1$s" class="widget %2$s">',
-      'after_widget'  => '</div>',
-      'before_title'  => '<h2>',
-      'after_title'   => '</h2>'
-    ));
-  }
-  
-	register_nav_menus(
-		array( 
-			'main_nav' => 'Main Menu'
-		)
-	);
 
-  class walker extends Walker_Nav_Menu {
-    // TODO: is this necessary?
+  
+  function theme_support() {
+      add_theme_support('automatic-feed-links');
+
+      add_theme_support('menus');
+      register_nav_menus(array( 
+        'main_nav'  => 'Main Menu'
+      ));
+    }
+    add_action('after_setup_theme','theme_support');	
+
+  class custom_walker extends Walker_Nav_Menu {
     function start_el(&$output, $item, $depth, $args) {
       global $wp_query;
-      $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
       $class_names = $value = '';
 
@@ -50,17 +41,17 @@
       $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
       $class_names = ' class="' . esc_attr( $class_names ) . '"';
 
-      $output .= $indent . '<li' . $value . $class_names .'>';
+      $output .= "\n<li" . $value . $class_names .'>';
 
       $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
       $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
       $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
       $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+      $attributes .= ! empty( $args->href_class ) ? ' class="'   . esc_attr( $args->href_class ) .'"' : '';
 
       $item_output = $args->before;
       $item_output .= '<a'. $attributes .'>';
       $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-      $item_output .= '<br /><span class="sub">' . $item->description . '</span>';
       $item_output .= '</a>';
       $item_output .= $args->after;
 
@@ -70,16 +61,57 @@
 
   function main_nav() {
     wp_nav_menu(array(
-      'menu'            => 'main_nav', /* menu name */
-      'theme_location'  => 'main_nav', /* where in the theme it's assigned */
+      'menu'            => 'main_nav',
+      'theme_location'  => 'main_nav',
+      'depth'           =>  1, // TODO: submenus
       'container'       => 'nav',
-      'container_class' => 'navigation-menu'
-      'menu_class'      => 'navigation-list',
-      'walker'          => new walker /* customizes the output of the menu */
+      'container_class' => 'header-navigation',
+      'href_class'      => 'menu-item-link',
+      'menu_class'      => 'navigation-menu h1',
+      'walker'          => new custom_walker
     ));
   }
 
-  add_theme_support( 'post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'audio', 'chat', 'video')); // Add 3.1 post format theme support.
+  class home_page_walker extends Walker_Nav_Menu {
+    function start_el(&$output, $item, $depth, $args) {
+      $output .= get_post_meta( $item->ID, '_menu_item_object_id', true );
+    }
+    function end_el(&$output, $item, $depth, $args) {
+      $output .= ',';
+    }
+  }
+
+  function home_pages() {
+      $page_ids = wp_nav_menu(array(
+        'menu'            => 'main_nav',
+        'theme_location'  => 'main_nav',
+        'depth'           => 1,
+        'container'       => false,
+        'items_wrap'      => '%3$s',
+        'walker'          => new home_page_walker,
+        'echo'            => false
+      ));
+
+      $page_ids = explode(',', $page_ids);
+
+      foreach ($page_ids as $page_id):
+      if(function_exists('iinclude_page')):
+        $title = get_the_title($page_id);
+        ?>
+        <section class="page-section" id="page-<?php the_slug($page_id); ?>">
+          <h1 class="section-name"><?php echo $title; ?></h1>
+          <div class="page-section-inner group">
+            <?php iinclude_page($page_id); ?>
+          </div>
+        </section>
+        <?php
+      endif;
+      endforeach;
+    }
+
+  function the_slug ($page_id) {
+      echo get_post($page_id)->post_name;
+    }
 
   function sendUaCompatible() {
       // https://github.com/h5bp/html5-boilerplate/issues/378
