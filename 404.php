6194a9951927f26c5ff9c9e7834e39ed45cff055
Casey Chow <casey.chow@gmail.com>
2012-06-26 16:50:50 -0400

    Insert 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
+  <section class="page-section">
+    <div class="page-section-inner group">
+      <article class="text-article" id="post-<?php the_ID(); ?>">
+        <img class="centered" src="<?php echo bloginfo('template_directory') . "/_/img/hal9000.png"; ?>" alt="" />
+        <h1>I'm sorry, Dave. I'm afraid I can't let  you do that.</h1>
+        <p>The page you requested does not currently exist. However, with a decent amount of time travel equipment, I am sure you may find the page in the future.</p>
+
+        <p>Please, sit back, relax, and surf the internet.</p>
+        
+        <?php get_search_form(); ?>
+      </article>
+    </div>
+  </section>
 
 <?php get_footer(); ?>
